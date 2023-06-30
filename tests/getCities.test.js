6324aa508bc5df48e6aeeb955c03012b18b32c85
Farid Diaz <farid.diaz@sansano.usm.cl>
2023-06-30 19:26:06 -0400

    Final commit

--- tests/getCities.test.js
+++ tests/getCities.test.js
@@ -1,4 +1,4 @@
-const { getAllCitiesUseCase, getCitiesByCountryUseCase } = require('../src/domain/cities/use_cases/getCities');
+const { getAllCitiesUseCase, getCitiesByCountryUseCase, getCitiesByCityNameAndCountryUseCase } = require('../src/domain/cities/use_cases/getCities');
 const worldCities = require('../dataset/world-cities_json.json');
 
 describe('Test para getCities', () => {
@@ -25,5 +25,62 @@ describe('Test para getCities', () => {
 
     // Comprobar
     expect(ctx.body).toEqual(citiesInMexico);
+  });
+
+  test('getCitiesByCountryUseCase debe devolver un mensaje si no se encuentran ciudades para un país dado', async () => {
+    // Preparar
+    const country = 'NonExistentCountry';
+    const ctx = { params: { country }, body: {} };
+    const expectedMessage = { message: "No se encontraron ciudades para el país ingresado" };
+
+    // Ejecutar
+    await getCitiesByCountryUseCase(ctx);
+
+    // Comprobar
+    expect(ctx.status).toEqual(200);
+    expect(ctx.body).toEqual(expectedMessage);
+  });
+
+  test('getCitiesByCountryUseCase debe devolver un mensaje si se ingresan caracteres numéricos', async () => {
+    // Preparar
+    const country = '123';
+    const ctx = { params: { country }, body: {} };
+    const expectedMessage = { message: "Solo se aceptan caracteres no numéricos" };
+
+    // Ejecutar
+    await getCitiesByCountryUseCase(ctx);
+
+    // Comprobar
+    expect(ctx.status).toEqual(400);
+    expect(ctx.body).toEqual(expectedMessage);
+  });
+
+  test('getCitiesByCityNameAndCountryUseCase debe devolver todas las ciudades para un país y ciudad dados', async () => {
+    // Preparar
+    const city = 'Mexico City';
+    const country = 'Mexico';
+    const ctx = { params: { city, country }, body: {} };
+    const cityInMexico = worldCities.find(c => c.name.toLowerCase() === city.toLowerCase() && c.country.toLowerCase() === country.toLowerCase());
+
+    // Ejecutar
+    await getCitiesByCityNameAndCountryUseCase(ctx);
+
+    // Comprobar
+    expect(ctx.body).toEqual(cityInMexico);
+  });
+
+  test('getCitiesByCityNameAndCountryUseCase debe devolver un mensaje si no se encuentran ciudades para un país y ciudad dados', async () => {
+    // Preparar
+    const city = 'NonExistentCity';
+    const country = 'NonExistentCountry';
+    const ctx = { params: { city, country }, body: {} };
+    const expectedMessage = { message: "No se encontraron ciudades para el país ingresado" };
+
+    // Ejecutar
+    await getCitiesByCityNameAndCountryUseCase(ctx);
+
+    // Comprobar
+    expect(ctx.status).toEqual(200);
+    expect(ctx.body).toEqual(expectedMessage);
   });
 });
